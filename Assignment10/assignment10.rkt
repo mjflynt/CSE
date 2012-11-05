c2c146662e4dadc9bede5b7037d8d44b4228f7d8
Jeffrey Flynt <mflynt@asu.edu>
2012-11-04 21:48:54 -0700

    Assignment 10 complete

--- Assignment10/assignment10.rkt
+++ Assignment10/assignment10.rkt
@@ -1,4 +1,8 @@
-; 
+; Assignment 10
+; Name:  Michael Jeffrey Flynt
+; email: mflynt@asu.edu
+
+; Description: Racket procedure find_min2 that reads 2 numbers from the keyboard, and returns the minimum of two.
 
 
 (define (find_min2 a b) 
