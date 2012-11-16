d2e45dde07e15fe2337360f3b0a7cd218e9f6235
Jeffrey Flynt <mflynt@asu.edu>
2012-11-15 19:43:03 -0700

    Assignment #16

---
+++ Assignment16/assignment16.rkt
@@ -0,0 +1,19 @@
+
+(define char_toupper (lambda (achar)
+                       (if (not (or (char<? achar #\a) (char>? achar #\z)))
+                           (integer->char (- (char->integer achar) (- (char->integer #\a) (char->integer #\A))))
+                           achar
+                           )
+                       )
+  )
+
+(define string_toupper (lambda (astring)
+                         (if (> (string-length astring)0)
+                             (string-append (string (char_toupper (string-ref astring 0))) (string_toupper (substring astring 1 (string-length astring))))
+                             ""
+                             ) 
+                         )
+  )
+
+(string_toupper (read-line))
+
